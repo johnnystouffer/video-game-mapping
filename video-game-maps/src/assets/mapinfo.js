62ba05ec3140b5e4eb4e50e6bd5922eb068cc9d0
Johnny Stouffer <stouff17@msu.edu>
2024-06-27 22:25:26 -0400

    Adding some simple info to a js file since we have not implemented MongoDB yet

---
+++ video-game-maps/src/assets/mapinfo.js
@@ -0,0 +1,28 @@
+const mapData = [
+    {
+        id: 'cap-kingdom', 
+        fullName: 'Cap Kingdom', 
+        game: "Super Mario Odyssey",
+        desc: "It is a gloomy, monochromatic kingdom filled with thick fog inhabited by hat-like beings called Bonneters, including Cappy and his sister, Tiara. The resident Bonneters excel at ship-building and can even attach sails to their homes to convert them into airships. This kingdom also serves as the tutorial stage of the game, and is the greater location of Bonneton",
+        image: "src/assets/cap-kingdom.jpg",
+        buttons: ['checkpoints', 'power-moon', 'multi-moon', 'purple-coins', 'bonus-areas']
+    },
+    {
+        id: 'cascade-kingdom', 
+        fullName: 'Cascade Kingdom', 
+        game: "Super Mario Odyssey",
+        desc: "It is the greater location of Fossil Falls, which is a prehistoric dinosaur-themed area. The main landmark of this kingdom is the waterfall flowing down from the skeleton of a Triceratops, while its background appears to be inspired from the real-life Iguazu Falls located between Argentina and Brazil",
+        image: "src/assets/cascade-kingdom.jpg",
+        buttons: ['checkpoints', 'power-moon', 'multi-moon', 'purple-coins', 'bonus-areas']
+    },
+    {
+        id: 'cascade-kingdom', 
+        fullName: 'Cascade Kingdom', 
+        game: "Super Mario Odyssey",
+        desc: "It is the greater location of Fossil Falls, which is a prehistoric dinosaur-themed area. The main landmark of this kingdom is the waterfall flowing down from the skeleton of a Triceratops, while its background appears to be inspired from the real-life Iguazu Falls located between Argentina and Brazil",
+        image: "src/assets/cascade-kingdom.jpg",
+        buttons: ['checkpoints', 'power-moon', 'multi-moon', 'purple-coins', 'bonus-areas']
+    },
+];
+
+export default mapData
