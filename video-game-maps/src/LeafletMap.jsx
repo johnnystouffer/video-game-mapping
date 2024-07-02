33b2d166654bf765ca7624eeafbe1872d8e905cf
Johnny Stouffer <stouff17@msu.edu>
2024-07-01 23:20:43 -0400

    Made map better

--- video-game-maps/src/LeafletMap.jsx
+++ video-game-maps/src/LeafletMap.jsx
@@ -16,8 +16,7 @@ const LeafletMap = (props) => {
                 center={[666, 660]} // Center the map based on image dimensions
                 zoom={0} // Initial zoom level
                 minZoom={-2} // Minimum zoom level
-                maxZoom={2} // Maximum zoom level
-                style={{ height: '100vh', width: '100vw' }} // Full viewport height and width
+                maxZoom={1} // Maximum zoom level
                 crs={L.CRS.Simple} // Use simple coordinate reference system
             >
                 <ImageOverlay
