c0611efa4050412e0066a807fd179254b0258a61
Johnny Stouffer <stouff17@msu.edu>
2024-07-01 23:37:46 -0400

    Added a tiny little icon

--- video-game-maps/src/LeafletMap.jsx
+++ video-game-maps/src/LeafletMap.jsx
@@ -1,14 +1,22 @@
 import React from 'react';
-import { MapContainer, ImageOverlay } from 'react-leaflet';
+import { MapContainer, ImageOverlay, Marker, Popup, } from 'react-leaflet';
 import "leaflet/dist/leaflet.css";
+import L from 'leaflet';
 import './LeafletMap.css';
 
 const LeafletMap = (props) => {
+
+
     const url = "/src/assets/maps/cap-kingdom.png"; 
     const bounds = [
         [0, 0],
         [1333, 1319]
     ];
+
+    const newIcon = new L.Icon({
+        iconUrl: "/src/assets/checkpoint.png",
+        iconSize: [35, 55]
+    })
 
     return (
         <>
@@ -23,6 +31,7 @@ const LeafletMap = (props) => {
                     url={url}
                     bounds={bounds}
                 />
+                <Marker position={[500, 500]} icon={newIcon}></Marker>
             </MapContainer>
         </>
     );
