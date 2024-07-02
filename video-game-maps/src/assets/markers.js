4542c5adb346a9ef7dfbf24cca6cd1c82b75513b
Johnny Stouffer <stouff17@msu.edu>
2024-07-02 00:01:40 -0400

    Added the first three markers!!! It is coming togetherrrr

---
+++ video-game-maps/src/assets/markers.js
@@ -0,0 +1,37 @@
+const checkpoint_markers = [
+    {
+        map: 'cap-kingdom',
+        iconUrl: '/src/assets/checkpoint.png',
+        position: [400, 350],
+        name: 'The Odyessey',
+        popupInfo: {
+            link: "",
+            description: "Where you spawn and leave in the game"
+        }
+    },
+    {
+        map: 'cap-kingdom',
+        iconUrl: '/src/assets/checkpoint.png',
+        position: [720, 582],
+        name: 'Central Plaza',
+        popupInfo: {
+            link: "",
+            description: "It is the central hub of the kingdom, specifically its residential district. Central Plaza is the first location that Mario visits alongside Cappy, where it is still ravaged from Bowser's attack on the kingdom. When Mario eventually revisits the Cap Kingdom, the Central Plaza is fully rebuilt, and a Crazy Cap shop and a Checkpoint Flag are present at the center of the area",
+
+
+        }
+    },
+    {
+        map: 'cap-kingdom',
+        iconUrl: '/src/assets/checkpoint.png',
+        position: [800, 910],
+        name: 'Top-Hat Tower',
+        popupInfo: {
+            link: "",
+            description: "Found in Bonneton. Inside it is the first platforming segment in the game, culminating in the game's first boss fight (against Topper) at the top.",
+
+
+        }
+    },
+]
+export default checkpoint_markers
