e0202b6c3a037d1ef2b38f11d4c58d0f1a978dbf
Johnny Stouffer <stouff17@msu.edu>
2024-07-08 21:29:19 -0400

    Finish current template of the data

--- video-game-maps/src/assets/markers.js
+++ video-game-maps/src/assets/markers.js
@@ -2,6 +2,7 @@ const checkpoint_markers = [
     {
         map: 'cap-kingdom',
         iconUrl: '/src/assets/checkpoint.png',
+        type: 'checkpoint',
         position: [400, 350],
         name: 'The Odyessey',
         popupInfo: {
@@ -13,6 +14,7 @@ const checkpoint_markers = [
         map: 'cap-kingdom',
         iconUrl: '/src/assets/checkpoint.png',
         position: [720, 582],
+        type: 'checkpoint',
         name: 'Central Plaza',
         popupInfo: {
             link: "",
@@ -26,6 +28,7 @@ const checkpoint_markers = [
         iconUrl: '/src/assets/checkpoint.png',
         position: [800, 910],
         name: 'Top-Hat Tower',
+        type: 'checkpoint',
         popupInfo: {
             link: "",
             description: "Found in Bonneton. Inside it is the first platforming segment in the game, culminating in the game's first boss fight (against Topper) at the top.",
