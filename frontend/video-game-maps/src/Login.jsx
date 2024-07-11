40f2170fd97e9b506315225d52bd3167431bc5ee
johnnystouffer <stouff17@msu.edu>
2024-07-11 13:48:42 -0700

    attempted JavaScript functions

--- frontend/video-game-maps/src/Login.jsx
+++ frontend/video-game-maps/src/Login.jsx
@@ -1,10 +1,27 @@
 import React from 'react'
 import "./Login.css"
+import { useState } from 'react'
 
 const Login = () => {
-
+    const [username, setUsername] = useState('');
+    const [firstGame, setFirstGame] = useState('');
+    const [favoriteGame, setFavoriteGame] = useState('');
+    const [dreamJob, setDreamJob] = useState('');
     
+    const createSaveState = () => {
+      const userData = {
+        user: username,
+        first_game: firstGame,
+        favorite_game: favoriteGame,
+        dream_job: dreamJob
+      }
 
+      
+    }
+
+    const forgotSaveState = () => {
+
+    }
 
   return (
     <>
@@ -21,16 +38,18 @@ const Login = () => {
             </div>
             <div className='sign-up'>
                 <h3> Create a Save State </h3>
-                <p> Enter your username in any game</p>
-                <input className='first-name-user' type="text" />
-                <p> What was your first video game? </p>
-                <input className='fav-job' type="text" />
-                <p> Enter Your Favorite Video Game: </p>
-                <input className='fav-game' type="text" />
-                <p> What is your dream job in the future? </p>
-                <input className='fav-job' type="text" /> <br />
-                <button id='make-state-button'> Create a Save State</button>
-                <button id='forgot-state-button'> Forgot My Save Key</button>
+                <form>
+                  <p> Enter your username in any game</p>
+                  <input className='first-name-user' type="text" onChange={(e) => { setUsername(e.target.value)}}/>
+                  <p> What was your first video game? </p>
+                  <input className='first-job' type="text" onChange={(e) => { setFirstGame(e.target.value)}}/>
+                  <p> Enter Your Favorite Video Game: </p>
+                  <input className='fav-game' type="text" onChange={(e) => { setFavoriteGame(e.target.value)}}/>
+                  <p> What is your dream job in the future? </p>
+                  <input className='fav-job' type="text" onChange={(e) => { setDreamJob(e.target.value)}}/> <br />
+                  <button id='make-state-button' onClick={createSaveState}> Create a Save State</button>
+                  <button id='forgot-state-button' onClick={forgotSaveState}> Forgot My Save Key</button>
+                </form>
             </div>
         </div>
         </div>
