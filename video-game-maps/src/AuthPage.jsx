3dc102dc7088e616d4d688641bed1dcab2606286
john-stouffer <john@apsdatatechnologies.com>
2025-05-15 18:19:40 -0400

    Finishing the visuals of front page and the authentication page

--- video-game-maps/src/AuthPage.jsx
+++ video-game-maps/src/AuthPage.jsx
@@ -16,21 +16,21 @@ const AuthPage = () => {
   };
 
   const handleGoogleLogin = () => {
-    alert('Google login clicked (hook up Firebase here)');
+    alert('Firebase Firebase, oooo fire fire fire firebase. POP, ba dum, dum dum');
   };
 
   return (
     <div className="auth-container">
       <div className="auth-card">
         <h2>{isLogin ? 'Log In' : 'Sign Up'}</h2>
-        <form onSubmit={handleSubmit}>
+        <form id='email-password-form' onSubmit={handleSubmit}>
           <input type="email" placeholder="Email" required />
           <input type="password" placeholder="Password" required />
-          {isLogin ? null : <input type='password' placeholder='Retype Password' required></input>}
-          <button id='sign-up-button'><Link  to={'/'}>{isLogin ? 'Log In' : 'Sign Up'}</Link></button>
+          {isLogin ? null : <input type='password' placeholder='Confirm Password' required></input>}
+          <button id='sign-up-button'><Link  to={'/'}>Submit</Link></button>
         </form>
-        <button className="google-button" onClick={handleGoogleLogin}>
-          Continue with Google
+        <button id="google-button" onClick={handleGoogleLogin}>
+          Continue with <img id='logo' src='../public/assets/googlelogo.png'></img>
         </button>
         <p onClick={toggleMode} className="toggle-link">
           {isLogin ? "Don't have an account? Sign up" : "Already have an account? Log in"}
