6dfbfde7a15e54873973c840963862dfb9c783e8
john-stouffer <john@apsdatatechnologies.com>
2025-06-05 19:38:53 -0500

    I forgot to update the repo noooo

--- video-game-maps/src/App.jsx
+++ video-game-maps/src/App.jsx
@@ -2,6 +2,7 @@ import SearchMaps from './components/SearchMaps.jsx'
 import Map from './components/Map.jsx'
 import AuthPage from './auth/AuthPage.jsx'
 import LogIn from './auth/LogIn.jsx'
+import UserProgress from './components/UserProgress.jsx'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 
 function App() {
@@ -14,6 +15,7 @@ function App() {
           <Route path='/:id' element={<Map/>}/>
           <Route path='/auth/signup' element={<AuthPage />} />
           <Route path='/auth/login' element={<LogIn />} />
+          <Route path='/progress' element={<UserProgress />} />
         </Routes>
       </BrowserRouter>
     </>
---
+++ video-game-maps/src/components/UserProgress.jsx
@@ -0,0 +1,55 @@
+import '../css/UserProgress.css';
+
+const userMap = {
+    "cap-kingdom": ["Cap Kingdom", 9, 10],
+    "cascade-kingdom": ["Cascade Kingdom", 19, 20],
+    "sand-kingdom": ["Sand Kingdom", 4, 30],
+    "lake-kingdom": ["Lake Kingdom", 1, 10],
+    "wooded-kingdom": ["Wooded Kingdom", 0, 10],
+    "snow-kingdom": ["Snow Kingdom", 10, 10],
+    "shiveria": ["Shiveria", 8, 12],
+    "deep-woods": ["Deep Woods", 9, 12],
+    "cloud-kingdom": ["Cloud Kingdom", 0, 10],
+    "lost-kingdom": ["Lost Kingdom", 3, 10],
+    "metro-kingdom": ["Metro Kingdom", 15, 92],
+    "seaside-kingdom": ["Seaside Kingdom", 10, 10],
+    "ruined-kingdom": ["Ruined Kingdom", 4, 10],
+    "moon-kingdom": ["Moon Kingdom", 23, 50],
+    "darker-side": ["Darker Side", 97, 100],
+    "dark-side": ["Dark Side", 1, 10],
+    "bowser-kingdom": ["Bowser Kingdom", 10, 10],
+    "mushroom-kingdom": ["Mushroom Kingdom", 9, 10],
+};
+
+
+
+const UserProgress = () => {
+
+    const sum = Array.from(Object.values(userMap)).reduce((acc, curr) => acc + curr[1], 0);
+    const total = Array.from(Object.values(userMap)).reduce((acc, curr) => acc + curr[2], 0);
+
+    return (
+        <div className="user-progress">
+            <h2 className="title-bar">USER PROGRESS</h2>
+            <div className='game-total'>
+                <p><b>Game Progress</b></p>
+                <p>{sum}/{total} ({((sum/total)*100).toFixed(1)}%)</p>
+                <div className='total-container'>
+                    <div className={((sum/total)*100) == 100 ? 'gold-bar' : 'actual-bar'} style={{width: (sum / total)*100 + '%'}}></div>
+                </div>
+            </div>
+            <div className="progress-bar-container">
+                {Object.keys(userMap).map((key) => (
+                    <div className='progress-bar' key={key}>
+                        <h3 className='map-title'>{userMap[key][0]} - {userMap[key][1]}/{userMap[key][2]}</h3>
+                        <div className='bar-container'>
+                            <div className={((userMap[key][1] / userMap[key][2])*100) == 100 ? 'gold-bar' : 'actual-bar'} style={{width: (userMap[key][1] / userMap[key][2])*100 + '%'}}></div>
+                        </div>
+                    </div>
+                ))}
+            </div>
+        </div>
+    );
+};
+
+export default UserProgress;
