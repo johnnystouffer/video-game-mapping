f0eb5615ec3235f593d1e1a5c9a02e72b3114fd5
johnnystouffer <stouff17@msu.edu>
2025-09-29 02:50:36 -0400

    Card and user progress redesign

--- video-game-maps/src/components/Card.jsx
+++ video-game-maps/src/components/Card.jsx
@@ -13,18 +13,17 @@ function Card(props) {
     };
 
     return (
-        <Link to={href}>
+        <div className='link-container'>
+            <Link to={href} className='card-link'>
+                <div className={props.game !== 'IN PROGRESS' ? 'card-container' : 'game-card-not-available'} style={cardStyle}>
+                    <div className='game-text-container'>
+                        <h2>{props.name}</h2>
+                        <h3>{props.game}</h3>
+                    </div>
+                </div>
 
-        <div className={props.game !== 'IN PROGRESS' ? 'card-container' : 'game-card-not-available'} style={cardStyle}>
-            <div className='game-text-container'>
-                <h2>{props.name}</h2>
-                <hr />
-                <h3>{props.game}</h3>
-            </div>
-
+            </Link>
         </div>
-
-        </Link>
     );
 }
 
